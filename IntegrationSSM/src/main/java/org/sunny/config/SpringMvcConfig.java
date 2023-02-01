cf686c68db8b4a3daa1bbb94d2762db23cd866f2
sunnytan53 <haoyuan.t@outlook.com>
2023-02-01 13:43:25 -0800

    Spring SSM interceptor

--- IntegrationSSM/src/main/java/org/sunny/config/SpringMvcConfig.java
+++ IntegrationSSM/src/main/java/org/sunny/config/SpringMvcConfig.java
@@ -1,11 +1,23 @@
 package org.sunny.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.sunny.controller.interceptor.ProjectInterceptor;
 
 @Configuration
 @ComponentScan("org.sunny.controller")
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books", "/books/*");
+    }
 }
